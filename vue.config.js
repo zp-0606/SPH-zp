9f3a51ea35629f846f5d54934ae1f111a295dfcc
zp-0606 <1123929807@qq.com>
2022-04-27 15:04:12 +0800

    修改接口

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://39.98.123.211'
+        target: 'http://gmall-h5-api.atguigu.cn'
         // pathRewrite: { '^/api': '' },
       },
     },
